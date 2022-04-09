3582e0d2864b108a79545acc9780de93998a0ad9
stcrult <82962821+stcrult@users.noreply.github.com>
2022-04-10 01:15:24 +0300

    Create Teacher.java

---
+++ StudentGradeSystem/Teacher.java
@@ -0,0 +1,11 @@
+public class Teacher {
+    String name;
+    String id;
+    String subject;
+
+    public Teacher(String name, String id, String subject) {
+        this.name = name;
+        this.id = id;
+        this.subject = subject;
+    }
+}
